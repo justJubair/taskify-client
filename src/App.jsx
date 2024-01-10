33fa138e9472f2a328d7fe76a649daea016cae33
Jubair <jubair.ahmed7946@gmail.com>
2024-01-10 22:33:44 +0600

    Add: data loaded from database

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    fetch("data.json")
+    fetch("http://localhost:5000/tasks")
       .then((res) => res.json())
       .then((data) => setTasks(data));
   }, []);
