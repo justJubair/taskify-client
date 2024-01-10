ced0553abb039bec98e527915ffa2ddcc90e668f
Jubair <jubair.ahmed7946@gmail.com>
2024-01-10 16:17:24 +0600

    Add: dummy cards body added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,54 @@
-
+// icons
+import { LuFileStack } from "react-icons/lu";
+import { FaCalendarAlt } from "react-icons/fa";
+import { FaComments } from "react-icons/fa";
 
 function App() {
-
-
   return (
     <>
-      
-      <h1>Vite + React</h1>
-      
-     
+      <div className="card bg-base-100 shadow-xl text-gray-500">
+        <div className="card-body">
+          <div className="flex items-center gap-2">
+            <img
+              className="w-12 rounded-full h-12 object-cover"
+              src="https://images.pexels.com/photos/4350178/pexels-photo-4350178.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt=""
+            />
+            <p className="font-bold">Card title!</p>
+          </div>
+
+          {/* container of task details and task completed */}
+          <div className="flex items-center justify-between mt-2">
+            {/* task details with icon */}
+            <div className="flex items-center gap-2 ">
+              <LuFileStack size={20} />
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+
+            {/* tasks completed */}
+            <div className="flex items-center gap-1 bg-base-200 p-1">
+              <FaCalendarAlt size={20} />
+              <p className="font-medium ">1/2</p>
+            </div>
+          </div>
+
+          {/* footer icons: attachments, comments and more */}
+          <div className="font-medium">
+            {/* total completed tasks */}
+            <p className="bg-base-200 p-1">12+</p>
+
+            {/* comments */}
+            <div className="flex items-center gap-1 bg-base-200 p-1">
+              <FaComments size={20} />
+              <p>15</p>
+            </div>
+
+            {/*  */}
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
